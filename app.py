018ac6f25878941484c0ceb0cba4d5961fa80add
big zed <bigzed224@gmail.com>
2026-03-03 03:03:47 -0800

    learnt how print wroks

---
+++ app.py
@@ -0,0 +1,9 @@
+# This is a comment
+# this is another comment
+# this is yet another
+trousers_quantity = 3 # number of trousers bought
+print(trousers_quantity,"trousers")
+trouser_cost = 750 # cost of one trouser
+print("each trouser costs",trouser_cost)
+total_cost = trouser_cost * trousers_quantity # total amount paid for trousers
+print("total cost of trousers",total_cost)
