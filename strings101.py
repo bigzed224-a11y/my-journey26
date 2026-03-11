6a2a7116adcbcb63d5a78cbc3c0ddd203b3c3dfa
big zed <bigzed224@gmail.com>
2026-03-11 04:03:40 -0700

    learn how to replace

--- strings101.py
+++ strings101.py
@@ -1,22 +1,12 @@
-# name = "manny"
-# surname = "babatunde"
-# age = 21
+# Sentence = """I love python.
+# Python is very easy, python is very powerful. AI depends mostly on python.
+# Python can be used for.
+# - Ai
+# - Web design
+# - Automations
+# Python Is a highly versatile language
+# """
+# print(Sentence.replace('python','java').replace('Python','java'))
 
-# print("my name is",name,"i am",age,"years old")
-# print(name+' '+str(age))
-
-# isstudent = False
-# print = (f"{{isstudent}} variable contains {len(str(isstudent))} characters") 
-
-# Math String Operation 
-Sentence = """I love python.
-Python is very easy, python is very powerful. AI depends mostly on python.
-Python can be used for.
-- Ai
-- Web design
-- Automations
-Python Is a highly versatile language
-"""
-#print(sentence)
-
-print(f"the word 'python' occour {Sentence.count('python')} times in the above sentence.")
+eurodate = "2026/03/11"
+# print(eurodate.replace("/","-"))
