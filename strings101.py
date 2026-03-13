32ac78eff1296d5c0da147ca85abeb67024309c8
big zed <bigzed224@gmail.com>
2026-03-13 08:35:34 -0700

    clear

--- strings101.py
+++ strings101.py
@@ -1,18 +1,24 @@
-# Sentence = """I love python.
-# Python is very easy, python is very powerful. AI depends mostly on python.
-# Python can be used for.
-# - Ai
-# - Web design
-# - Automations
-# Python Is a highly versatile language
-# """
-# print(Sentence.replace('python','java').replace('Python','java'))
+Sentence = """I love python.
+Python is very easy, python is very powerful. AI depends mostly on python.
+Python can be used for.
+- Ai
+- Web design
+- Automations
+Python Is a highly versatile language
+"""
+print(Sentence.replace('python','java').replace('Python','java'))
 
-name = "manny"
-surname = "eze"
-age = 35
-fullname = name+' '+surname
+# name = "manny"
+# surname = "eze"
+# age = 35
+# fullname = name+' '+surname
 
 # eurodate = "2026/03/11"
-# print(eurodate.replace("/","-"))
-print(f"my name is {fullname}.I am {age}years old.\nIn the next ten years,i will be {age+10}years old")
+# # print(eurodate.replace("/","-"))
+# print(f"my name is {fullname}.I am {age}years old.\nIn the next ten years,i will be {age+10}years old")
+
+# timestamp1 = '2026-12-23'
+
+# customerinfo ="mary-23-female-kogi"
+# print(customerinfo)
+# print()
