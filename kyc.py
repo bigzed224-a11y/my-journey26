1b01ddaefbca1bc5505dd17287e67ee3ecdad14a
big zed <bigzed224@gmail.com>
2026-03-17 02:44:13 -0700

    i modified two files

--- kyc.py
+++ kyc.py
@@ -23,33 +23,18 @@
 #           -> Occupation: {occupation}\t
 #           -> tribe : {tribe}
 # """)
-
-
-
-def get_valid_input(prompt, validation_type):
-    while True:
-        user_input = input(prompt).strip()
-        
-        if validation_type == "alpha":
-            if user_input.isalpha():
-                return user_input.capitalize()
-            print("Invalid input: Please use letters only.")
-            
-        elif validation_type == "digit":
-            if user_input.isdigit() and int(user_input) > 0:
-                return user_input
-            print("Invalid input: Please enter a valid positive number.")
+ 
 
 # --- Main Program ---
 print("--- Customer Profile Setup ---")
 
-first_name = get_valid_input("Enter First Name: ", "alpha")
-last_name  = get_valid_input("Enter Last Name: ", "alpha")
-age        = get_valid_input("Enter Age: ", "digit")
-gender = get_valid_input("are you male or female?:", "alpha")
-state = get_valid_input("enter your state of origin:", "alpha")
-occupation = get_valid_input("what is your occupation?:", "alpha")
-tribe = get_valid_input("what nigeria tribe are you from?:", "alpha")
+first_name = input("Enter First Name: ")
+last_name  = input("Enter Last Name: ")
+age        = input("Enter Age: ")
+gender = input("are you male or female?:")
+state = input("enter your state of origin:")
+occupation = input("what is your occupation?:")
+tribe = input("what nigeria tribe are you from?:")
 
 print("\n--- Profile Created Successfully ---")
 print(f"Name: {first_name} {last_name}")
